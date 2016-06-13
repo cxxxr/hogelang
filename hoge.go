6ab5508c0676c2cb56d7f0d2dc68f258138b41f8
cxxxr <g23tlm@gmail.com>
2016-06-14 00:21:01 +0900

    コマンドライン引数でファイルを指定できるようにした

--- hoge.go
+++ hoge.go
@@ -6,37 +6,56 @@ import (
 	"github.com/cxxxr/hogelang/compiler"
 	"github.com/cxxxr/hogelang/parser"
 	"github.com/cxxxr/hogelang/vm"
+	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
+func eval(m *vm.Machine, exe *vm.Executable, str string) vm.Object {
+	p := parser.NewParser(strings.NewReader(str))
+	x, err := p.Parse()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	} else {
+		exe = compiler.Compile(exe, x)
+		m.SetExecutable(exe)
+		return vm.Run(m)
+	}
+}
 
+func main() {
 	exe := vm.NewExecutable(nil)
 	m := vm.NewMachine(exe)
 	vm.Init(m)
 
-	for {
-		fmt.Print("> ")
-		line, _, err := reader.ReadLine()
-
-		if err != nil {
-			break
+	if len(os.Args) > 1 {
+		for _, file := range os.Args[1:] {
+			dat, err := ioutil.ReadFile(file)
+			if err != nil {
+				log.Fatal(err)
+			}
+			eval(m, exe, string(dat))
 		}
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			fmt.Print("> ")
+			line, _, err := reader.ReadLine()
 
-		if len(line) == 0 {
-			continue
-		}
+			if err != nil {
+				break
+			}
 
-		p := parser.NewParser(strings.NewReader(string(line)))
-		x, err := p.Parse()
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			exe = compiler.Compile(exe, x)
-			m.SetExecutable(exe)
-			fmt.Println(vm.Run(m))
+			if len(line) == 0 {
+				continue
+			}
+
+			v := eval(m, exe, string(line))
+			if v != nil {
+				fmt.Println(v)
+			}
 		}
 	}
 }
